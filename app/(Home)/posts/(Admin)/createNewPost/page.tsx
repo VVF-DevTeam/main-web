8fa99f0c6947f8db4967490b4acd875809772385
jayantpuri <jayantpuri16@gmail.com>
2025-01-13 11:45:20 -0800

    change3

--- app/(Home)/posts/(Admin)/createNewPost/page.tsx
+++ app/(Home)/posts/(Admin)/createNewPost/page.tsx
@@ -1,15 +1,16 @@
 import CreatePostForm from './_components/CreatePostForm'
-// import { auth } from '@/auth'
-// import { redirect } from 'next/navigation'
+import { auth } from '@/auth'
+import { redirect } from 'next/navigation'
 const NewPost = async () => {
-  // const session = await auth()
-  // if (!session?.user?.id) {
-  //   return redirect('/app/signIn')
-  // }
+  // TODO: check if user is logged in and send user data
+  const session = await auth()
+  if (!session?.user?.id) {
+    return redirect('/app/signIn')
+  }
  
   return (
     <div className="mx-auto my-40 w-full max-w-5xl p-8 lg:p-12 xl:p-16">
-      <CreatePostForm author={"session?.user?.id"} />
+      <CreatePostForm author={session?.user?.id} />
     </div>
   )
 }
