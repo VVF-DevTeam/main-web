567efba146d3e4812fdeb9d8087bc9253959261a
dad13 <dad13@sfu.ca>
2025-01-18 11:37:15 -0800

    Fix code for accessibility

--- app/[locale]/(Home)/_components/contact.tsx
+++ app/[locale]/(Home)/_components/contact.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { Contact2 } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
 
+<<<<<<< HEAD
 const Contact = () => {
   // @ts-ignore: useTranslation will always throw an error for typescript
   const { t } = useTranslation()
@@ -12,6 +13,11 @@ const Contact = () => {
     window.location.href = 'https://www.instagram.com/vietvibe.foundation'
   }
 
+=======
+const Contact = async ({ locale }: { locale: string }) => {
+  const { t } = await initTranslation(locale, ['homePage', 'common']);
+
+>>>>>>> df05fc2 (Fix code for accessibility)
   return (
     <div className="h-[50vh] w-full bg-[#1B171A]/50">
       <div className="flex h-full w-full flex-col items-center justify-center gap-y-14 lg:gap-y-20">
@@ -20,19 +26,28 @@ const Contact = () => {
             {t('getInTouch-contact')}
           </h2>
         </div>
+<<<<<<< HEAD
         <button
           className="inline-flex items-center rounded-md border-b-2 border-[#EFB9A2] bg-[#1B171A] px-14 py-6 font-bold tracking-wide shadow-md hover:border-[#EFB9A2]/80 hover:bg-[#1B171A]/90 hover:text-white"
           onClick={handleClick}
         >
           <a className="mx-auto flex items-center gap-x-4 text-[#EFB9A2]">
+=======
+        <a
+          href="https://www.instagram.com/vietvibe.foundation"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center rounded-md border-b-2 border-[#EFB9A2] bg-[#1B171A] px-14 py-6 font-bold tracking-wide shadow-md hover:border-[#EFB9A2]/80 hover:bg-[#1B171A]/90 hover:text-white"
+        >
+          <span className="mx-auto flex items-center gap-x-4 text-[#EFB9A2]">
+>>>>>>> df05fc2 (Fix code for accessibility)
             {t('chat-contact')}
-
             <Contact2 className="h-7 w-7" />
-          </a>
-        </button>
+          </span>
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
