1be67c9bc3e4179aa1df534972c7615031e402dc
jayantpuri16 <jayantpuri16@gmail.com>
2025-03-12 17:54:11 +0530

    fix build error

--- app/[locale]/components/SunEditor.tsx
+++ app/[locale]/components/SunEditor.tsx
@@ -1,8 +1,7 @@
 'use client'
 import React from 'react'
-import { useRef } from 'react'
+// import { useRef } from 'react'
 import dynamic from 'next/dynamic'
-import { buttonList } from 'suneditor-react'
 import 'suneditor/dist/css/suneditor.min.css' // Import Sun Editor's CSS File
 const SunEditor = dynamic(() => import('suneditor-react'), {
   ssr: false,
@@ -14,10 +13,10 @@ interface EditorProps {
 }
 
 const MyComponent = ({ onChange, value }: EditorProps) => {
-  const editorRef = useRef()
-  const getSunEditorInstance = (sunEditor: any) => {
-    editorRef.current = sunEditor
-  }
+  // const editorRef = useRef()
+  // const getSunEditorInstance = (sunEditor) => {
+  //   editorRef.current = sunEditor
+  // }
   return (
     <div>
       <p> My Other Contents </p>
@@ -25,8 +24,6 @@ const MyComponent = ({ onChange, value }: EditorProps) => {
         defaultValue="<p>The editor's default value</p>"
         width="100%"
         height="100%"
-        getSunEditorInstance={getSunEditorInstance}
-        autoFocus={true}
         onChange={onChange}
         setContents={value}
         setOptions={{
