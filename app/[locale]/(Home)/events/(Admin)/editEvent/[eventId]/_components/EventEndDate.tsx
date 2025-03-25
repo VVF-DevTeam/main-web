8d4e2e153c8de6e32ed0133d049839728c7444fd
Khai Hung <hungluongkhai@gmail.com>
2025-03-25 01:29:20 -0700

    Fix DatePicker import not found bug

--- app/[locale]/(Home)/events/(Admin)/editEvent/[eventId]/_components/EventEndDate.tsx
+++ app/[locale]/(Home)/events/(Admin)/editEvent/[eventId]/_components/EventEndDate.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react'
-import DatePicker from '@/app/[locale]/components/DatePicker'
+import DatePicker from '@/components/ui/DatePicker'
 import { Event } from '@prisma/client'
 import { useRouter } from 'next/navigation'
 import { useToast } from '@/hooks/use-toast'
