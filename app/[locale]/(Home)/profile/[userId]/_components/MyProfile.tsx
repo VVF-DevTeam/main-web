0f4c1fec75857191b1d0e390e0a7bac97fc4f743
Khai Hung <hungluongkhai@gmail.com>
2025-03-25 19:39:39 -0700

    Enhance profile UI

--- app/[locale]/(Home)/profile/[userId]/_components/MyProfile.tsx
+++ app/[locale]/(Home)/profile/[userId]/_components/MyProfile.tsx
@@ -30,7 +30,7 @@ const MyProfile = async ({
 
   console.log(user?.image)
   return (
-    <div className="min-h-screen bg-gray-50 p-4 md:p-8">
+    <div className="min-h-screen p-4 md:p-8">
       <div className="mx-auto max-w-7xl">
         <div className="mb-8 flex items-center justify-between">
           <h1 className="text-3xl font-bold">{t('my-profile')}</h1>
