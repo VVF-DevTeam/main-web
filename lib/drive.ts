15d775dae744d823cddae43ba36188277be1c164
Khai Hung <hungluongkhai@gmail.com>
2025-04-09 03:25:48 -0700

    Add API for list and upload avatars to GG drive

---
+++ lib/drive.ts
@@ -0,0 +1,71 @@
+import { google } from 'googleapis'
+import { Readable } from 'stream'
+// import mime from 'mime-types';
+// import fs from 'fs/promises';
+
+const FOLDER_ID = '1S2Y8zt25b47LeIsGVLhFojLMQwa2UDM6' // replace with actual ID
+
+const auth = new google.auth.GoogleAuth({
+  credentials: JSON.parse(process.env.GOOGLE_DRIVE_SERVICE_ACCOUNT_KEY!),
+  scopes: ['https://www.googleapis.com/auth/drive'],
+})
+
+const drive = google.drive({ version: 'v3', auth })
+
+export async function listAvatars() {
+  const res = await drive.files.list({
+    q: `'${FOLDER_ID}' in parents and trashed = false`,
+    fields: 'files(id, name)',
+    supportsAllDrives: true,
+    includeItemsFromAllDrives: true,
+  })
+
+  return (
+    res.data.files?.map((file) => ({
+      id: file.id,
+      name: file.name,
+      
+      url: `https://drive.google.com/thumbnail?id=${file.id}&sz=w500`, // change width here if needed
+    })) || []
+  )
+}
+
+export async function uploadAvatar(
+  fileBuffer: Buffer,
+  filename: string,
+  mimetype: string
+) {
+  // Working code
+  // await drive.files.create({
+  //     requestBody: {
+  //       name: 'test-avatar.jpg',
+  //       parents: [FOLDER_ID],
+  //     },
+  //     media: {
+  //       mimeType: 'image/jpeg',
+  //       body: 'Asdasd',
+  //     },
+  //     supportsAllDrives: true,
+  //   })
+
+  // Need testing code
+  const stream = Readable.from(fileBuffer)
+
+  const fileRes = await drive.files.create({
+    requestBody: {
+      name: filename,
+      parents: [FOLDER_ID],
+    },
+    media: {
+      mimeType: mimetype,
+      body: stream,
+    },
+    fields: 'id, name',
+  })
+
+  return {
+    id: fileRes.data.id,
+    name: fileRes.data.name,
+    url: `https://drive.google.com/uc?export=view&id=${fileRes.data.id}`,
+  }
+}
