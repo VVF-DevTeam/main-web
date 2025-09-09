ebeb9d4b41781924693d243c916e31adf3bcf4f3
kuro2904 <trung2001sgp@gmail.com>
2025-09-09 16:25:16 +0700

    fix build

--- firebaseServer.ts
+++ firebaseServer.ts
@@ -1,13 +1,21 @@
 import admin from 'firebase-admin'
 
+const serviceAccountString = process.env
+  .FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY as string
+
+if (!serviceAccountString)
+  throw new Error('Missing environment variables for firebase backend')
+
+const serviceAccount = JSON.parse(serviceAccountString)
+
+let firebaseAdmin: admin.app.App
+
 if (!admin.apps.length) {
-  const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY!
-
-  const serviceAccount = JSON.parse(serviceAccountString)
-
-  admin.initializeApp({
+  firebaseAdmin = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   })
+} else {
+  firebaseAdmin = admin.app()
 }
 
-export const firebaseAdmin = admin
+export { firebaseAdmin }
