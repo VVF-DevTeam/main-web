1c62babe9121a9c16a1696e8d5fb789ac7e7f327
Khai Hung <hungluongkhai@gmail.com>
2025-12-29 08:13:22 +0700

    Add code for debugging stripe webhook

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -176,7 +176,8 @@ export async function POST(req: NextRequest) {
     }
 
     console.log('metadata', metadata)
-
+    console.log('paymentData', paymentData)
+    
     // in case paymentId is not found, set it to an empty string, then admin can ask devs to fix it
     if (!paymentId) {
       paymentId = ''
