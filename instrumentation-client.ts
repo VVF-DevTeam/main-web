ba79fca6e2a59b53814c5f98d6c58fcd66fc8e43
Khai Hung <hungluongkhai@gmail.com>
2025-12-29 15:09:55 +0700

    Add Sentry for errors monitor

---
+++ instrumentation-client.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://2e45987640d9603e69f8d5fd7a7e6828@o4510616399708160.ingest.us.sentry.io/4510616401281024",
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
---
+++ sentry.server.config.ts
@@ -0,0 +1,13 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://2e45987640d9603e69f8d5fd7a7e6828@o4510616399708160.ingest.us.sentry.io/4510616401281024",
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
