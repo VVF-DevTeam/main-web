619941d4c9abc2c1a3958d2969204b02b9d8b746
Khai Hung <hungluongkhai@gmail.com>
2025-12-30 14:08:54 +0700

    Update businessData with new partner names and links for membership discounts

--- app/[locale]/(Home)/registration/_components/_membership/businessData.ts
+++ app/[locale]/(Home)/registration/_components/_membership/businessData.ts
@@ -2,8 +2,8 @@
 // You can move this to a config file or database later
 export const businessData = [
   { name: 'Lululemon Canada', discount: 'Up to 15%', link: 'https://shop.lululemon.com/en-ca/' },
-  { name: 'Concept Studio', discount: '50%', link: '' },
-  { name: 'Family Photo Shoot at An Nhien Studio', discount: 'Up to 50%', link: '' },
+  { name: 'Anh Concept Studio - Business brand photo & video', discount: '50%', link: 'https://www.instagram.com/anh.concept/' },
+  { name: 'An Nhien Studio - Wedding photo & video', discount: 'Up to 50%', link: 'https://www.instagram.com/annhienstudio/' },
   { name: 'Cozoro Home - Rent Apartment in Vietnam', discount: 'Up to 10%', link: 'https://cozorohome.com' },
   { name: 'Legend Dessert', discount: 'Up to 10%', link: 'https://www.instagram.com/freshlegendbyelizasweet' },
   { name: 'Kitchen Katherina', discount: 'Up to 10%', link: 'https://www.facebook.com/kitchenkatherina' },
